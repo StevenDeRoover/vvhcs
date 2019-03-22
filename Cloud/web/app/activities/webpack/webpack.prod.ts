3631aed03a15d9afbfd146b1ffb7dc31b57df06c
stevendr <stevenderoover@gmail.com>
2019-03-22 12:06:32 +0100

    test

---
+++ Cloud/web/app/activities/webpack/webpack.prod.ts
@@ -0,0 +1,22 @@
+import merge from 'webpack-merge';
+
+import common from './webpack.common';
+
+import { optimizeJS } from './plugins/uglify';
+import { optimizeCss } from './plugins/css';
+
+export default merge(common, {
+	mode: 'production',
+	performance: {
+		hints: false,
+	},
+	optimization: {
+		splitChunks: {
+			chunks: "initial",
+		},
+	},
+	plugins: [
+		optimizeCss,
+		optimizeJS,
+	],
+});
